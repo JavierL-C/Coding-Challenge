e6637be08262b5eb83ffcabb73c871915e0cf603
Javier Lumbi <javierlumbi23@gmail.com>
2022-12-23 11:28:44 -0600

    add APIManager file

---
+++ Coding-Challenge/Networking/API/APIManager.swift
@@ -0,0 +1,15 @@
+//
+//  APIManager.swift
+//  Coding-Challenge
+//
+//  Created by Javier  on 23/12/22.
+//
+
+import Foundation
+import UIKit
+import Combine
+
+final class APIManager {
+    static let baseServiceURL = "https://api.themoviedb.org/3/"
+    static let apiKey = "b60f6e6a79a5b0ba7002d8dad8e72d8a"
+}
